cd1d890968202ecb02f49d23b90a78b82a175f86
Lucas Verdi <lucas.a.verdi@gmail.com>
2024-01-16 17:48:39 -0300

    Comunicação estabelecida com o front

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('ecommerce', 'root', '64784585', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+app.use(express.json());
+app.use(cors({
+  origin: true,
+}))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +21,15 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/teste', async (req, res) => {
+  const clientes = await clientes.create({
+    nome: req.body.nome,
+    sobrenome: req.body.sobrenome,
+    dtnascimento: req.body.dtnascimento,
+    cpf: req.body.cpf,
+    email: req.body.email,
+    celular: req.body.celular
+  })
 })
