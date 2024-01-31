ef08245912c76015b4dabd7edda03168f7069f70
Lucas Verdi <lucas.a.verdi@gmail.com>
2024-01-31 17:56:13 -0300

    Autenticação com JWT

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const { Sequelize, DataTypes } = require('sequelize')
+const jwt = require('jsonwebtoken')
+const segredo = 'yN5"xD7!dM9<fF8!eO6!tF5}sD9"kZ0,'
 
+
+function verifyJWT(req, res, next){
+  const token = req.headers['x-access-token']
+  jwt.verify(token, segredo, (err, decoded) => {
+    if (err) return res.status(401).end()
+    req.userId = decoded.userId
+    next()
+  })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -64,11 +75,18 @@ app.post('/cadastro', async (req, res) => {
 app.post('/login', async (req, res) => {
   const { email, password } = req.body
   const logado = await Cliente.findOne({ where: {email, password} })
+  const id = await Cliente.findAll({attributes:['id'], where: {email, password}})
 
   if(logado){
-    res.json({ success: true })
+    const token = jwt.sign({userId: id}, segredo, {expiresIn: 30})
+    res.json({ success: true, token })
   }
   else{
     res.json({ success: false })
   }
 })
+
+app.post('/verify', verifyJWT, async (req, res) => {
+  res.json({ success: true })
+})
+
